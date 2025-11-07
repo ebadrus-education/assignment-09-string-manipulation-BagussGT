c4fa7198aedb0a8c47abb36736fb4182e55e53aa
BagussGT <bimabimbim800@gmail.com>
2025-11-07 09:24:39 +0700

    Tugas02.cpp

---
+++ 02_append/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+int main(){
+  string nama;
+cout << nama;
+cout << "Nama kamu siapa sayang?? ";
+getline (cin.nama) ;
+cout << " I LOVE YOUUUUU"<< nama <<endl;
+}
